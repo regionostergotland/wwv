aeefca50b791421bd60809cdf3674d1694690067
AlexVestin <aleve030@student.liu.se>
2019-02-26 14:33:41 +0100

    formatting, error handling and readme update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 
-#World Wide Vård
+# World Wide Vård
 Lägg till API-nyckel i apikey.js
 
+```
 npm install --save
 npm start
+```
--- app.js
+++ app.js
@@ -1,55 +1,68 @@
 window.onload = function() {
-    const SCOPES = [
-        "https://www.googleapis.com/auth/fitness.activity.read",
-        "https://www.googleapis.com/auth/fitness.activity.write",
-        "https://www.googleapis.com/auth/fitness.blood_pressure.read",
-        "https://www.googleapis.com/auth/fitness.body.read"
-      ].join(" ");
-      
-      
-      let accessToken = "";
-      
-      function gapiAllStreams(res) {
-        const endTime = String(Math.floor(Date.now() * Math.pow(10, 6)));
-        const dataSetId = "0-"+endTime;
+  const SCOPES = [
+    "https://www.googleapis.com/auth/fitness.activity.read",
+    "https://www.googleapis.com/auth/fitness.activity.write",
+    "https://www.googleapis.com/auth/fitness.blood_pressure.read",
+    "https://www.googleapis.com/auth/fitness.body.read"
+  ].join(" ");
 
-        res.dataSource.forEach(source => {
-            if(source.dataStreamId.split(":")[0] === "raw") {
-                const url = "https://www.googleapis.com/fitness/v1/users/me/dataSources/" + source.dataStreamId + "/datasets/" + dataSetId + "?access_token="+ accessToken;
-                fetch(url).then(response => response.json())
-                .then(json => console.log(json.dataSourceId, json.point))
-                .catch(err => console.log(err.message))
-            }
-        })
-      }
-      
-      
-      function gapiAuthorizationResponse(response) {
-        if (response.error) {
-          console.log(response.error, "err");
-          return;
-        }
-        accessToken = response.access_token;
-        fetch("https://www.googleapis.com/fitness/v1/users/me/dataSources?access_token="+accessToken)
-        .then(res => res.json())
-        .then(gapiAllStreams);
-        
-      }
-      
-      function gapiLoaded() {
-            const params = {
-                client_id:              API_KEY,
-                cookie_policy:          'single_host_origin',
-                fetch_basic_profile:    true,
-                ux_mode:                "popup",
-                include_granted_scopes: true,
-                prompt:                 '',
-                scope:                  SCOPES,
-          };
-      
-          gapi.auth2.authorize(params, gapiAuthorizationResponse);
-      }
+  let accessToken = "";
 
-      gapiLoaded();
-}
+  function gapiAllStreams(res) {
+    // UNIX start and end times.
+    const endTime = String(Math.floor(Date.now() * Math.pow(10, 6)));
+    const dataSetId = "0-" + endTime;
 
+    // Iterate the datasources, and log the result to the console
+    res.dataSource.forEach(source => {
+      if (source.dataStreamId.split(":")[0] === "raw") {
+        const url =
+          "https://www.googleapis.com/fitness/v1/users/me/dataSources/" +
+          source.dataStreamId +
+          "/datasets/" +
+          dataSetId +
+          "?access_token=" +
+          accessToken;
+        fetch(url)
+          .then(response => response.json())
+          .then(json => console.log(json.dataSourceId, json.point))
+          .catch(err => console.log(err.message));
+      }
+    });
+  }
+
+  function gapiAuthorizationResponse(response) {
+    if (response.error) {
+      console.log(response.error, "err");
+      return;
+    }
+    accessToken = response.access_token;
+    fetch(
+      "https://www.googleapis.com/fitness/v1/users/me/dataSources?access_token=" +
+        accessToken
+    )
+      .then(res => res.json())
+      .then(gapiAllStreams);
+  }
+
+  function gapiLoaded() {
+    const params = {
+      client_id: API_KEY,
+      cookie_policy: "single_host_origin",
+      fetch_basic_profile: true,
+      ux_mode: "popup",
+      include_granted_scopes: true,
+      prompt: "",
+      scope: SCOPES
+    };
+
+    gapi.auth2.authorize(params, gapiAuthorizationResponse);
+  }
+
+  if (!API_KEY) {
+    alert("Add the api key to apikey.js");
+    return;
+  }
+
+  gapiLoaded();
+};
