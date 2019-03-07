0646531f1327f97b2196d17ff6281ecc7b33cd89
Mattias Salo <matsa159@student.liu.se>
2019-03-07 10:15:08 +0100

    Add component for list view of health data points

---
+++ src/app/health-list-items/health-list-items.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-health-list-items',
+  templateUrl: './health-list-items.component.html',
+  styleUrls: ['./health-list-items.component.scss']
+})
+export class HealthListItemsComponent implements OnInit {
+  listItems: ListItem[] = [
+    {parts: [
+        {comp: 2, data: 'Blodtryck'},
+        {comp: 2, data: '140/80'},
+        {comp: 1, data: 'Sittande, liggande'}
+      ]
+    },
+    {parts: [
+        {comp: 2, data: 'Blodtryck'},
+        {comp: 2, data: '120/80'},
+        {comp: 1, data: 'Sittande, liggande'}
+      ]
+    },
+    {parts: [
+        {comp: 2, data: 'Blodtryck'},
+        {comp: 2, data: '130/85'},
+        {comp: 1, data: 'Sittande, liggande'}
+      ]
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+export class ListItemPart {
+  comp: number;
+  data: string;
+}
+
+export class ListItem {
+  parts: ListItemPart[];
+}
