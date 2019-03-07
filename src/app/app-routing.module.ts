efa72c57abed29b494cba1443e84aac693fd2512
sofieliljedahl <sofli181@student.liu.se>
2019-03-07 11:44:32 +0100

    added sidebar route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { WwvHomeComponent } from './wwv-home/wwv-home.component';
 import { WwvSourcesComponent } from './wwv-sources/wwv-sources.component';
 import { WwvCategoryPickerComponent } from './wwv-category-picker/wwv-category-picker.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ListViewComponent } from './list-view/list-view.component';
 
 const routes: Routes = [
   { path: 'home', component: WwvHomeComponent },
   { path: 'sources', component: WwvSourcesComponent },
   { path: 'catpicker', component: WwvCategoryPickerComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'sidebar/:id', component: SidebarComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes), RouterModule ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
