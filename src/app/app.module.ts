d9069a0315d3ca4dccb7d9aff7cd6b52a472e45b
sofieliljedahl <sofli181@student.liu.se>
2019-03-07 11:55:13 +0100

    fixed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,68 +2,72 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { WwvCategoryPickerComponent } from './wwv-category-picker/wwv-category-picker.component';
-import { WwvToolbarComponent } from './wwv-toolbar/wwv-toolbar.component';
-import { WwvSourcesComponent } from './wwv-sources/wwv-sources.component';
-import { WwvHomeComponent } from './wwv-home/wwv-home.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SourcesComponent } from './sources/sources.component';
+import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CategoryPickerComponent } from './category-picker/category-picker.component';
+import { InfoComponent } from './info/info.component';
+import { HelpComponent } from './help/help.component';
+import { HealthListItemsComponent } from './health-list-items/health-list-items.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {
-  MatAutocompleteModule,
-  MatBadgeModule,
-  MatBottomSheetModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatTreeModule,
-  MatFormFieldModule,
+import { MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    MatFormFieldModule } from '@angular/material';
 
-} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WwvToolbarComponent,
-    WwvHomeComponent,
-    WwvSourcesComponent,
-    WwvCategoryPickerComponent,
-    SidebarComponent,
-    ListViewComponent
+    ToolbarComponent,
+    HomeComponent,
+    SourcesComponent,
+    CategoryPickerComponent,
+    InfoComponent,
+    HelpComponent,
+    HealthListItemsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
