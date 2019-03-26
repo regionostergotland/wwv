35c0e5f2504b5dec3815cae496ae04b9857238a3
sofieliljedahl <sofli181@student.liu.se>
2019-03-26 14:15:41 +0100

    added integration with conveyor and ehrservice for category-tester + unit test

--- src/app/category-picker/category-picker.component.spec.ts
+++ src/app/category-picker/category-picker.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
+import { EhrService } from '../ehr.service';
 
 import {
   MatSelectModule,
@@ -23,6 +24,7 @@ import { By } from '@angular/platform-browser';
 describe('CategoryPickerComponent', () => {
   let component: CategoryPickerComponent;
   let fixture: ComponentFixture<CategoryPickerComponent>;
+  let ehrService: EhrService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -43,7 +45,7 @@ describe('CategoryPickerComponent', () => {
         MatButtonModule,
         MatButtonToggleModule,
         MatNativeDateModule
-      ]
+      ],
     })
     .compileComponents();
   }));
@@ -51,11 +53,24 @@ describe('CategoryPickerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CategoryPickerComponent);
     component = fixture.componentInstance;
+    ehrService = TestBed.get(EhrService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should pair categories with their correct labels', () => {
+    expect(component.categories.length).toBeGreaterThan(0);
+    for (const i in component.categories) {
+      if (component.categories.hasOwnProperty(i)) {
+        const label1: string = component.categories[i][1];
+        const category1: string = component.categories[i][0];
+        const label: string = ehrService.getCategorySpec(category1).label;
+        expect(label1).toEqual(label);
+      }
+    }
   });
 
   // it('should click change value', async(() => {
--- src/app/category-picker/category-picker.component.ts
+++ src/app/category-picker/category-picker.component.ts
@@ -1,50 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import {DateAdapter} from '@angular/material/core';
+import { Conveyor } from '../conveyor.service';
+import { EhrService } from '../ehr.service';
+import { CategorySpec } from '../shared/spec';
 
 @Component({
   selector: 'app-category-picker',
   templateUrl: './category-picker.component.html',
   styleUrls: ['./category-picker.component.scss']
 })
+
 export class CategoryPickerComponent implements OnInit {
 
-  constructor(private adapter: DateAdapter<any>) {
+  constructor(private adapter: DateAdapter<any>, private conveyor: Conveyor, private ehrService: EhrService) {
     this.adapter.setLocale('sv');
   }
 
   chosenCategories: string[] = [];
+  categories: [string, string][] = [];
+  categoryIds: string[] = [];
   startDate: Date;
   endDate: Date;
 
-  categories: string[] = [
-    'blood pressure',
-    'weight',
-    'steps',
-    'blood sugar',
-    'something else',
-    'blood pressure',
-    'weight',
-    'steps',
-    'blood sugar',
-    'something else',
-    'blood pressure',
-    'weight',
-    'steps',
-    'blood sugar',
-    'something else',
-    'blood pressure',
-    'weight',
-    'steps',
-    'blood sugar',
-    'something else',
-    'blood pressure',
-    'weight',
-    'steps',
-    'blood sugar',
-    'something else'
-  ];
+  // categories: string[] = [
+  //   'blood pressure',
+  //   'weight',
+  //   'steps',
+  //   'blood sugar',
+  //   'something else',
+  //   'blood pressure',
+  //   'weight',
+  //   'steps',
+  //   'blood sugar',
+  //   'something else',
+  //   'blood pressure',
+  //   'weight',
+  //   'steps',
+  //   'blood sugar',
+  //   'something else',
+  //   'blood pressure',
+  //   'weight',
+  //   'steps',
+  //   'blood sugar',
+  //   'something else',
+  //   'blood pressure',
+  //   'weight',
+  //   'steps',
+  //   'blood sugar',
+  //   'something else'
+  // ];
 
   ngOnInit() {
+    this.categoryIds = this.conveyor.getCategories('google-fit');
+    this.getCategories();
+  }
+
+  /**
+   * Adds all category labels together with the ids, in a list.
+   */
+  getCategories() {
+    let cat: CategorySpec;
+    for (const entry of this.categoryIds) {
+      cat = this.ehrService.getCategorySpec(entry);
+      this.categories.push([entry, cat.label]);
+    }
   }
 
   /*
