dde58199e1f2df3abad724c05cd1e0bab43cf229
Mattias Salo <matsa159@student.liu.se>
2019-03-28 11:41:27 +0100

    Add link between sources-component and category-picker

--- src/app/sources/sources.component.ts
+++ src/app/sources/sources.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Source } from '../source';
 import { Conveyor } from '../conveyor.service';
 
+const googleFit = 'google-fit';
+const withings = 'withings';
+
 @Component({
   selector: 'app-sources',
   templateUrl: './sources.component.html',
@@ -12,12 +15,14 @@ export class SourcesComponent implements OnInit {
   sources: Source[] = [];
 
   availableSources: Map<string, Source> = new Map<string, Source>([
-    ['google-fit', {
+    [googleFit, {
+      id: googleFit,
       name: 'Google Fit',
       imageUrl: 'https://www.gstatic.com/images/branding/product/1x/gfit_512dp.png',
       routerLink: '/catpicker'
     }],
-    ['withings', {
+    [withings, {
+      id: withings,
       name: 'Withings',
       imageUrl: 'http://resources.mynewsdesk.com/image/upload/c_limit,dpr_1.0,f_auto,h_700,q_auto,w_690/jymhygjz5t7hzld9qe6j.jpg',
       routerLink: '/catpicker'
@@ -38,6 +43,7 @@ export class SourcesComponent implements OnInit {
         this.sources.push(this.availableSources.get(platform));
       } else {
         this.sources.push({
+          id: platform,
           name: platform,
           imageUrl: '',
           routerLink: '/catpicker/'
