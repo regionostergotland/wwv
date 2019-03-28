ab51429dc682c85c4510b9cf6af70e64a810370a
Mattias Salo <matsa159@student.liu.se>
2019-03-28 11:26:09 +0100

    Connect source-picker with conveyor

--- src/app/sources/sources.component.ts
+++ src/app/sources/sources.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Source } from '../source';
+import { Conveyor } from '../conveyor.service';
 
 @Component({
   selector: 'app-sources',
@@ -8,22 +9,40 @@ import { Source } from '../source';
 })
 export class SourcesComponent implements OnInit {
 
-  sources: Source[] = [
-    {
+  sources: Source[] = [];
+
+  availableSources: Map<string, Source> = new Map<string, Source>([
+    ['google-fit', {
       name: 'Google Fit',
       imageUrl: 'https://www.gstatic.com/images/branding/product/1x/gfit_512dp.png',
       routerLink: '/catpicker'
-    },
-    {
+    }],
+    ['withings', {
       name: 'Withings',
       imageUrl: 'http://resources.mynewsdesk.com/image/upload/c_limit,dpr_1.0,f_auto,h_700,q_auto,w_690/jymhygjz5t7hzld9qe6j.jpg',
       routerLink: '/catpicker'
-    }
-  ];
+    }]]);
 
-  constructor() { }
+  constructor(private conveyor: Conveyor) {
+    this.addSources();
+  }
 
   ngOnInit() {
   }
 
+  addSources() {
+    const platforms = this.conveyor.getPlatforms();
+    this.sources = [];
+    for (const platform of platforms) {
+      if (this.availableSources.has(platform)) {
+        this.sources.push(this.availableSources.get(platform));
+      } else {
+        this.sources.push({
+          name: platform,
+          imageUrl: '',
+          routerLink: '/catpicker/'
+        });
+      }
+    }
+  }
 }
