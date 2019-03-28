4618e145aeff346f5442bd95f442f2397661849c
Mattias Salo <matsa159@student.liu.se>
2019-03-28 23:21:54 +0100

    add comments to the health-list

--- src/app/health-list-items/health-list-items.component.ts
+++ src/app/health-list-items/health-list-items.component.ts
@@ -17,10 +17,20 @@ export class HealthListItemsComponent implements OnInit {
 
   categories: string[] = [];
 
+  /**
+   * Gets a string representation of the date correctly formatted to be read by a human.
+   * @param date the date to format
+   * @returns a formatted string representing a date
+   */
   static getDate(date: Date): string {
     return date.toLocaleDateString('sv-SE');
   }
 
+  /**
+   * Gets a string representation of the time correctly formatted to be read by a human.
+   * @param date the date to get the time from to format
+   * @returns a formatted string representing a time
+   */
   static getTime(date: Date): string {
     return date.toLocaleTimeString('sv-SE');
   }
@@ -43,17 +53,25 @@ export class HealthListItemsComponent implements OnInit {
     return this.conveyor.getDataList(category).getPoints();
   }
 
-  getlabel(category: string, labelId: string): string {
+  /**
+   * Gets the label of the id specified.
+   * @param category the category in which the id is in
+   * @param labelId the id to fetch the label to
+   * @returns a label for the specified id
+   */
+  getLabel(category: string, labelId: string): string {
     if (labelId === 'date') {
       return 'Datum';
     }
     return this.conveyor.getDataList(category).getDataType(labelId).label;
   }
 
-  /*
-  * Returns the columns which should be displayed in the table depending on which
-  * category it is
-  */
+  /**
+   * Returns the columns which should be displayed in the table depending on which
+   * category it is.
+   * @param category the category to be displayed
+   * @returns a list of labels for the specified category
+   */
   getDisplayedColumns(category: string): string[] {
     const result: string[] = [];
     if (this.getData(category)) {
@@ -69,6 +87,12 @@ export class HealthListItemsComponent implements OnInit {
     return result;
   }
 
+  /**
+   * Gets the type to display in the table.
+   * @param category the category to fetch types from
+   * @param key the id to get the label from
+   * @returns the type to display in the table
+   */
   getVisualType(category: string, key: string): DataTypeEnum {
     if (key === 'date') {
       return DataTypeEnum.DATE_TIME;
@@ -76,14 +100,23 @@ export class HealthListItemsComponent implements OnInit {
     return this.conveyor.getDataList(category).spec.dataTypes.get(key).type;
   }
 
-  /*
+  /**
    * Gets the options to choose from according to a DataType.
-   * */
+   * @param category the category to fetch options from
+   * @param key the id to to fetch the options from
+   * @returns a list of options
+   */
   getOptions(category: string, key: string): DataTypeCodedTextOpt[] {
     const datatypes: DataTypeCodedText = this.conveyor.getDataList(category).getDataType(key) as DataTypeCodedText;
     return datatypes.options;
   }
 
+  /**
+   * Gets the data to be displayed from the point
+   * @param point the datapoint to get the data from
+   * @param key the data category to get
+   * @returns a string of the value to show
+   */
   getPointData(point: DataPoint, key: string): string {
     if (key === 'date') {
       return HealthListItemsComponent.getDate(point.get('time'));
@@ -94,16 +127,24 @@ export class HealthListItemsComponent implements OnInit {
     return point.get(key);
   }
 
-  getNumberOfValues(category: string) {
-    return this.conveyor.getDataList(category).getPoints().length;
-  }
-
+  /**
+   * Sets the data category in the dataPoint to the set option
+   * @param key the data category to set
+   * @param point the point to set data in
+   * @param option the option to set
+   */
   setOption(key: string, point: DataPoint, option: string) {
     point.set(key, option);
     this.a = this.a + 1;
     console.log(Array.from(point.keys()));
   }
 
+  /**
+   * Set all unset data types in a category
+   * @param key the data category to be set
+   * @param option the option to set
+   * @param category the category the points are in
+   */
   setAllOptions(key: string, option: string, category: string) {
     let allData = true;
     for (const point of this.getData(category)) {
