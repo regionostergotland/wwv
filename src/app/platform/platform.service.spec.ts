98f5e496facfce39ca01c6ba4103c619ad4fa65e
Noah Hellman <noah.hellman@protonmail.com>
2019-04-03 16:19:33 +0200

    fix test: add Map to providers for platform

--- src/app/platform/platform.service.spec.ts
+++ src/app/platform/platform.service.spec.ts
@@ -3,7 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { Platform } from './platform.service';
 
 describe('Platform', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      Map,
+    ]
+  }));
 
   it('should be created', () => {
     const service: Platform = TestBed.get(Platform);
