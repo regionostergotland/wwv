49fecd9dd610cd385e0829c738b7bcb332af648b
Mattias Salo <matsa159@student.liu.se>
2019-04-04 13:25:08 +0200

    inspection: fix imports in test file so that tests passes

--- src/app/gui/inspection/inspection.component.spec.ts
+++ src/app/gui/inspection/inspection.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatExpansionModule, MatMenuModule, MatTableModule, MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatExpansionModule, MatMenuModule, MatTableModule, MatPaginatorModule } from '@angular/material';
 import { InspectionComponent } from './inspection.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -31,9 +31,9 @@ describe('InspectionComponent', () => {
       imports: [ MatExpansionModule,
         MatMenuModule,
         MatTableModule,
-        MatPaginator,
-        MatTableDataSource,
+        MatPaginatorModule,
         BrowserAnimationsModule,
+        MatTableModule,
 
         GoogleApiModule.forRoot({
           provide: NG_GAPI_CONFIG,
