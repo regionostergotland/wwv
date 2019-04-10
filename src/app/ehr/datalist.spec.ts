d07babd5932684720d54c0ed85a9494551994c8c
Noah Hellman <noah.hellman@protonmail.com>
2019-04-10 16:38:42 +0200

    add tests for interval

--- src/app/ehr/datalist.spec.ts
+++ src/app/ehr/datalist.spec.ts
@@ -12,260 +12,187 @@ describe('Ehr Types', () => {
     ]
   }));
 
-  const categories: CategorySpec[] = [
-    {
-      id : 'blood_pressure',
-      label : 'Blodtryck',
-      description : 'Mätning av arteriellt blodtryck.',
-      dataTypes : new Map<string, DataType>([
-        [
-          'time',
-          new DataTypeDateTime(
-            ['any_event'],
-            'Tid',
-            'Tidpunkt vid mätning',
-            true, false,
-          )
-        ],
-        [
-          'systolic',
-          new DataTypeQuantity(
-            ['any_event'],
-            'Övertryck',
-            'Systoliskt övertryck av blod',
-            true, false,
-            'mm[Hg]', 0, 1000,
-          )
-        ],
-        [
-          'diastolic',
-          new DataTypeQuantity(
-            ['any_event'],
-            'Undertryck',
-            'Diastoliskt undertryck av blod',
-            true, false,
-            'mm[Hg]', 0, 1000
-          )
-        ],
-        [
-          'position',
-          new DataTypeCodedText(
-            ['any_event'],
-            'Position',
-            'Position vid mätning.',
-            false, false,
-            [
-              {
-                code: 'at1000',
-                label: 'Stående',
-                description: 'Stående under mätning.'
-              },
-              {
-                code: 'at1001',
-                label: 'Sittande',
-                description: 'Sittande under mätning.'
-              },
-              {
-                code: 'at1003',
-                label: 'Liggande',
-                description: 'Liggande under mätning.'
-              }
-            ]
-          )
-        ],
-      ])
-    },
-    {
-      id : 'body_weight',
-      label : 'Kroppsvikt',
-      description : 'Mätning av faktisk kroppsvikt.',
-      dataTypes : new Map<string, DataType>([
-        [
-          'time',
-          new DataTypeDateTime(
-            ['any_event'],
-            'Tid',
-            'Tidpunkt vid mätning',
-            true, false,
-          )
-        ],
-        [
-          'weight',
-          new DataTypeQuantity(
-            ['any_event'],
-            'Vikt',
-            'Kroppsvikt',
-            true, false,
-            'kg', 0, 1000
-          )
-        ],
-        [
-          'state_of_dress',
-          new DataTypeCodedText(
-            ['any_event'],
-            'Klädsel',
-            'Klädsel vid mätning.',
-            false, false,
-            [
-              {
-                code: 'at0011',
-                label: 'Lättklädd/underkläder',
-                description: 'Klädsel som ej bidrar med vikt.'
-              },
-              {
-                code: 'at0013',
-                label: 'Naken',
-                description: 'Helt utan kläder.'
-              },
-              {
-                code: 'at0010',
-                label: 'Fullklädd',
-                description: 'Klädsel som bidrar med vikt.'
-              }
-            ]
-          )
-        ],
-      ])
-    }
-  ];
-
-  /*
-   * Test that splits datapoints into width interval, passes if date count is
-   * correct.
-   */
-  it('should split datapoints into one day intervals', () => {
-    const test = new DataList(categories[0]);
-    const date: Date = new Date();
-    const date1: Date = new Date();
-    const date2: Date = new Date();
-    const date3: Date = new Date();
-    const date4: Date = new Date();
-    const date5: Date = new Date();
-    date.setDate(1);
-    date1.setDate(1);
-    date2.setDate(2);
-    date3.setDate(2);
-    date4.setDate(3);
-    date5.setDate(3);
-    date.setHours(10);
-    date1.setHours(11);
-    date2.setHours(12);
-    date3.setHours(13);
-    date4.setHours(6);
-    date5.setHours(7);
-    test.addPoints([
-        new DataPoint(
-            [
-                [ 'time', date ],
-                [ 'systolic', 101 ],
-                [ 'diastolic', 20 ],
-            ]
-        ),
-        new DataPoint(
-            [
-                [ 'time', date1 ],
-                [ 'systolic', 10 ],
-                [ 'diastolic', 22 ],
-                [ 'position', 'at1003'],
-            ]
-        ),
-        new DataPoint(
-            [
-                [ 'time', date2 ],
-                [ 'systolic', 100 ],
-                [ 'diastolic', 20 ],
-            ]
-        ),
-        new DataPoint(
-            [
-                [ 'time', date3 ],
-                [ 'systolic', 110 ],
-                [ 'diastolic', 220 ],
-                [ 'position', 'at1001'],
-            ]
-        ),
-        new DataPoint(
-            [
-                [ 'time', date4 ],
-                [ 'systolic', 10 ],
-                [ 'diastolic', 2 ],
-            ]
-        ),
-        new DataPoint(
-            [
-                [ 'time', date5 ],
-                [ 'systolic', 118 ],
-                [ 'diastolic', 232 ],
-                [ 'position', 'at1003'],
-            ]
+  const bloodpressure: CategorySpec = {
+    id : 'blood_pressure',
+    label : 'Blodtryck',
+    description : 'Mätning av arteriellt blodtryck.',
+    dataTypes : new Map<string, DataType>([
+      [
+        'time',
+        new DataTypeDateTime(
+          ['any_event'],
+          'Tid',
+          'Tidpunkt vid mätning',
+          true, false,
         )
-    ]);
-    test.setInterval(1000 * 3600 * 24, MathFunctionEnum.MEAN);
-    expect(test.getPoints().length).toEqual(3);
-  });
-
-  it('should create new datapoints with mean of the interval points numerized fields', () => {
-    const test = new DataList(categories[1]);
-    const date: Date = new Date();
-    const date1: Date = new Date();
-    const date2: Date = new Date();
-    const date3: Date = new Date();
-    const date4: Date = new Date();
-    const date5: Date = new Date();
-    date.setDate(1);
-    date1.setDate(1);
-    date2.setDate(1);
-    date3.setDate(2);
-    date4.setDate(3);
-    date5.setDate(3);
-    date.setHours(2);
-    date1.setHours(3);
-    date2.setHours(4);
-    date3.setHours(5);
-    date4.setHours(6);
-    date5.setHours(7);
-    test.addPoints([
-      new DataPoint(
-        [
-          ['time', date],
-          ['weight', 90],
-          ['state_of_dress', 'at0011'],
-        ]
-      ),
-      new DataPoint(
-        [
-          ['time', date1],
-          ['weight', 70],
-        ]
-      ),
+      ],
+      [
+        'systolic',
+        new DataTypeQuantity(
+          ['any_event'],
+          'Övertryck',
+          'Systoliskt övertryck av blod',
+          true, false,
+          'mm[Hg]', 0, 1000,
+        )
+      ],
+      [
+        'diastolic',
+        new DataTypeQuantity(
+          ['any_event'],
+          'Undertryck',
+          'Diastoliskt undertryck av blod',
+          true, false,
+          'mm[Hg]', 0, 1000
+        )
+      ],
+      [
+        'position',
+        new DataTypeCodedText(
+          ['any_event'],
+          'Position',
+          'Position vid mätning.',
+          false, false,
+          [
+            {
+              code: 'at1000',
+              label: 'Stående',
+              description: 'Stående under mätning.'
+            },
+            {
+              code: 'at1001',
+              label: 'Sittande',
+              description: 'Sittande under mätning.'
+            },
+            {
+              code: 'at1003',
+              label: 'Liggande',
+              description: 'Liggande under mätning.'
+            }
+          ]
+        )
+      ],
+    ])
+  };
+  const bodyweight: CategorySpec = {
+    id : 'body_weight',
+    label : 'Kroppsvikt',
+    description : 'Mätning av faktisk kroppsvikt.',
+    dataTypes : new Map<string, DataType>([
+      [
+        'time',
+        new DataTypeDateTime(
+          ['any_event'],
+          'Tid',
+          'Tidpunkt vid mätning',
+          true, false,
+        )
+      ],
+      [
+        'weight',
+        new DataTypeQuantity(
+          ['any_event'],
+          'Vikt',
+          'Kroppsvikt',
+          true, false,
+          'kg', 0, 1000
+        )
+      ],
+      [
+        'state_of_dress',
+        new DataTypeCodedText(
+          ['any_event'],
+          'Klädsel',
+          'Klädsel vid mätning.',
+          false, false,
+          [
+            {
+              code: 'at0011',
+              label: 'Lättklädd/underkläder',
+              description: 'Klädsel som ej bidrar med vikt.'
+            },
+            {
+              code: 'at0013',
+              label: 'Naken',
+              description: 'Helt utan kläder.'
+            },
+            {
+              code: 'at0010',
+              label: 'Fullklädd',
+              description: 'Klädsel som bidrar med vikt.'
+            }
+          ]
+        )
+      ],
+    ])
+  };
+  const dataList = new DataList(bloodpressure);
+  dataList.addPoints([
       new DataPoint(
           [
-            ['time', date2],
-            ['weight', 80],
+              [ 'time', new Date(2016, 1, 1) ],
+              [ 'systolic', 101 ],
+              [ 'diastolic', 20 ],
           ]
       ),
       new DataPoint(
           [
-            ['time', date3],
-            ['weight', 40],
+              [ 'time', new Date(2016, 1, 1) ],
+              [ 'systolic', 103 ],
+              [ 'diastolic', 22 ],
+              [ 'position', 'at1003'],
           ]
       ),
       new DataPoint(
           [
-            ['time', date4],
-            ['weight', 80],
+              [ 'time', new Date(2016, 1, 2) ],
+              [ 'systolic', 100 ],
+              [ 'diastolic', 20 ],
           ]
       ),
       new DataPoint(
           [
-            ['time', date5],
-            ['weight', 100],
+              [ 'time', new Date(2016, 1, 2) ],
+              [ 'systolic', 110 ],
+              [ 'diastolic', 220 ],
+              [ 'position', 'at1001'],
+          ]
+      ),
+      new DataPoint(
+          [
+              [ 'time', new Date(2016, 1, 3) ],
+              [ 'systolic', 10 ],
+              [ 'diastolic', 2 ],
+              [ 'position', 'at1003'],
+          ]
+      ),
+      new DataPoint(
+          [
+              [ 'time', new Date(2016, 1, 3) ],
+              [ 'systolic', 118 ],
+              [ 'diastolic', 232 ],
+              [ 'position', 'at1003'],
           ]
       )
-    ]);
-    test.setInterval(1000 * 3600 * 24, MathFunctionEnum.MAX);
-    const res: DataPoint[] = test.getPoints();
-    expect(res[0].get('weight')).toEqual(90);
+  ]);
+
+  /*
+   * Test that datalist splits datapoints into width interval
+   */
+  it('should split datapoints into one day intervals, producing three points', () => {
+    dataList.setInterval(1000 * 3600 * 24, MathFunctionEnum.MEAN);
+    expect(dataList.getPoints().length).toEqual(3);
+  });
+  it('should create new datapoints with mean of the interval points numerized fields', () => {
+    dataList.setInterval(1000 * 3600 * 24, MathFunctionEnum.MEAN);
+    expect(dataList.getPoints()[0].get('systolic')).toEqual(102);
+    expect(dataList.getPoints()[0].get('diastolic')).toEqual(21);
+  });
+  it('should keep coded text if all values are the same', () => {
+    expect(dataList.getPoints()[2].get('position')).toEqual('at1003');
+  });
+  it('should remove coded text if all values are not the same', () => {
+    expect(dataList.getPoints()[0].get('position')).toEqual(undefined);
   });
 
   /**
