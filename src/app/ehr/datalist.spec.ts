8ed3ce086f82986e0aa0d716aaf171462553338b
Noah Hellman <noah.hellman@protonmail.com>
2019-04-11 14:16:39 +0200

    add tests for samePeriod

--- src/app/ehr/datalist.spec.ts
+++ src/app/ehr/datalist.spec.ts
@@ -180,11 +180,11 @@ describe('Ehr Types', () => {
    * Test that datalist splits datapoints into width interval
    */
   it('should split datapoints into one day intervals, producing three points', () => {
-    dataList.setInterval(1000 * 3600 * 24, MathFunctionEnum.MEAN);
+    dataList.setInterval(PeriodWidths.DAY, MathFunctionEnum.MEAN);
     expect(dataList.getPoints().length).toEqual(3);
   });
   it('should create new datapoints with mean of the interval points numerized fields', () => {
-    dataList.setInterval(1000 * 3600 * 24, MathFunctionEnum.MEAN);
+    dataList.setInterval(PeriodWidths.DAY, MathFunctionEnum.MEAN);
     expect(dataList.getPoints()[0].get('systolic')).toEqual(102);
     expect(dataList.getPoints()[0].get('diastolic')).toEqual(21);
   });
@@ -232,7 +232,7 @@ describe('Ehr Types', () => {
   /*
    * Test that start of period is correct and according to the locale.
    */
-  fit('should be the start of the year', () => {
+  it('should be the start of the year', () => {
     let date = new Date();
     date.setFullYear(2019, 0, 1);
     date.setHours(0, 0, 0, 0);
@@ -242,7 +242,7 @@ describe('Ehr Types', () => {
         .getTime())
           .toBe(startOf2019);
   });
-  fit('should be the start of the month', () => {
+  it('should be the start of the month', () => {
     let date = new Date();
     date.setFullYear(2019, 3, 1);
     date.setHours(0, 0, 0, 0);
@@ -252,7 +252,7 @@ describe('Ehr Types', () => {
         .getTime())
           .toBe(startOfApril);
   });
-  fit('should be the start of the week', () => {
+  it('should be the start of the week', () => {
     let date = new Date();
     date.setFullYear(2019, 3, 8);
     date.setHours(0, 0, 0, 0);
@@ -263,7 +263,7 @@ describe('Ehr Types', () => {
     //  .getTime())
     //   .toBe(startOfWeek);
   });
-  fit('should be the start of the day', () => {
+  it('should be the start of the day', () => {
     let date = new Date();
     date.setFullYear(2019, 3, 11);
     date.setHours(0, 0, 0, 0);
@@ -273,7 +273,7 @@ describe('Ehr Types', () => {
         .getTime())
           .toBe(startOfDay);
   });
-  fit('should be the start of the hour', () => {
+  it('should be the start of the hour', () => {
     let date = new Date();
     date.setFullYear(2019, 3, 11);
     date.setHours(10, 0, 0, 0);
@@ -282,6 +282,128 @@ describe('Ehr Types', () => {
     expect(DataPoint.startOfPeriod(date, PeriodWidths.HOUR)
         .getTime())
           .toBe(startOfDay);
+  });
+
+  /**
+   * Test that points withing the same period are considered so.
+   */
+  it('should match points within the same year', () => {
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(2019, 1, 20)),
+      new Date(new Date().setFullYear(2019, 0, 1)),
+      PeriodWidths.YEAR
+    )).toBeTruthy();
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(1970, 1, 20)),
+      new Date(new Date().setFullYear(1970, 11, 31)),
+      PeriodWidths.YEAR
+    )).toBeTruthy();
+  });
+  it('should not match points not within the same year', () => {
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(2018, 11, 31)),
+      new Date(new Date().setFullYear(2019, 0, 1)),
+      PeriodWidths.YEAR
+    )).toBeFalsy();
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(2070, 11, 31)),
+      new Date(new Date().setFullYear(1970, 11, 31)),
+      PeriodWidths.YEAR
+    )).toBeFalsy();
+  });
+  it('should match points within the same month', () => {
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(2019, 0, 31)),
+      new Date(new Date().setFullYear(2019, 0, 1)),
+      PeriodWidths.MONTH
+    )).toBeTruthy();
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(1970, 11, 1)),
+      new Date(new Date().setFullYear(1970, 11, 31)),
+      PeriodWidths.MONTH
+    )).toBeTruthy();
+  });
+  it('should not match points not within the same month', () => {
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(2019, 1, 1)),
+      new Date(new Date().setFullYear(2019, 0, 1)),
+      PeriodWidths.MONTH
+    )).toBeFalsy();
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(1970, 1, 1)),
+      new Date(new Date().setFullYear(1970, 11, 31)),
+      PeriodWidths.MONTH
+    )).toBeFalsy();
+  });
+  it('should match points within the same week', () => {
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(2019, 3, 14)),
+      new Date(new Date().setFullYear(2019, 3, 8)),
+      PeriodWidths.WEEK
+    )).toBeTruthy();
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(2018, 11, 31)),
+      new Date(new Date().setFullYear(2019, 0, 6)),
+      PeriodWidths.WEEK
+    )).toBeTruthy();
+  });
+  it('should not match points not within the same week', () => {
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(2019, 3, 14)),
+      new Date(new Date().setFullYear(2019, 3, 15)),
+      PeriodWidths.WEEK
+    )).toBeFalsy();
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setFullYear(1970, 1, 1)),
+      new Date(new Date().setFullYear(1971, 1, 1)),
+      PeriodWidths.WEEK
+    )).toBeFalsy();
+  });
+  it('should match points within the same day', () => {
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setHours(0, 0, 0, 0)),
+      new Date(new Date().setHours(23, 59, 59, 999)),
+      PeriodWidths.DAY
+    )).toBeTruthy();
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setHours(12, 34, 31, 324)),
+      new Date(new Date().setHours(15, 23, 29, 972)),
+      PeriodWidths.DAY
+    )).toBeTruthy();
+  });
+  it('should not match points not within the same day', () => {
+    let day1 = new Date(2018, 11, 31);
+    let day2 = new Date(2019, 0, 1);
+    expect(DataPoint.samePeriod(day1, day2, PeriodWidths.DAY)).toBeFalsy();
+    expect(DataPoint.samePeriod(
+      new Date(day1.setHours(23, 59, 59, 999)),
+      new Date(day2.setHours(0, 0, 0, 0)),
+      PeriodWidths.DAY
+    )).toBeFalsy();
+  });
+  it('should match points within the same hour', () => {
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setHours(23, 0, 0, 0)),
+      new Date(new Date().setHours(23, 59, 59, 999)),
+      PeriodWidths.HOUR
+    )).toBeTruthy();
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setHours(15, 34, 31, 324)),
+      new Date(new Date().setHours(15, 23, 29, 972)),
+      PeriodWidths.HOUR
+    )).toBeTruthy();
+  });
+  it('should not match points not within the same hour', () => {
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setHours(23, 59, 59, 999)),
+      new Date(new Date().setHours(0, 0, 0, 0)),
+      PeriodWidths.HOUR
+    )).toBeFalsy();
+    expect(DataPoint.samePeriod(
+      new Date(new Date().setHours(1, 0, 0, 0)),
+      new Date(new Date().setHours(0, 0, 0, 0)),
+      PeriodWidths.HOUR
+    )).toBeFalsy();
   });
 
   /**
