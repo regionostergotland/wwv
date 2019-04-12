58b5ca1dec76e42979d78a5c98c74a6f013b7bdb
Noah Hellman <noah.hellman@protonmail.com>
2019-04-12 14:55:31 +0200

    update constructor in test

--- src/app/ehr/datalist.spec.ts
+++ src/app/ehr/datalist.spec.ts
@@ -330,7 +330,16 @@ describe('datalist', () => {
     const spec: CategorySpec = {
       id : 'id', label : '', description : '',
       dataTypes : new Map<string, DataType>([
-        [ 'time', new DataTypeDateTime(['any_event'], '', '', true, false) ],
+        [ 'time', new DataTypeDateTime(
+          {
+            path: ['any_event'],
+            label: '',
+            description: '',
+            required: true,
+            single: false,
+            visible: true
+          })
+        ],
       ])
     };
     const list = new DataList(spec);
