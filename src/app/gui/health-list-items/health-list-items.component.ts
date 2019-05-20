2dee0665ba0b925529baaf5c1b71921e43b42bbc
David <davidlantz96@gmail.com>
2019-05-20 11:08:01 +0200

    Change some text, add icon for removal

--- src/app/gui/health-list-items/health-list-items.component.ts
+++ src/app/gui/health-list-items/health-list-items.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewChild, Inject} from '@angular/core';
+import {Component, Input, OnInit, ViewChild, Inject, ViewChildren, AfterViewInit, QueryList} from '@angular/core';
 import { CategorySpec,
          DataTypeCodedText,
          DataTypeCodedTextOpt,
@@ -161,11 +161,11 @@ export class HealthListItemsComponent implements OnInit {
 
   mathOptions: Map<MathFunctionEnum, string> =
     new Map<MathFunctionEnum, string>([
-    [MathFunctionEnum.MAX, ', maximalt värde '],
-    [MathFunctionEnum.MEAN, ', medelvärde '],
-    [MathFunctionEnum.MEDIAN, ', median '],
-    [MathFunctionEnum.MIN, ', minimalt värde '],
-    [MathFunctionEnum.TOTAL, ', totala värde '],
+    [MathFunctionEnum.MAX, 'maximalt värde '],
+    [MathFunctionEnum.MEAN, 'medelvärde '],
+    [MathFunctionEnum.MEDIAN, 'median '],
+    [MathFunctionEnum.MIN, 'minimalt värde '],
+    [MathFunctionEnum.TOTAL, 'totala värde '],
   ]);
 
   intervalOptions: Map<PeriodWidths, string> = new Map<PeriodWidths, string>([
@@ -176,7 +176,8 @@ export class HealthListItemsComponent implements OnInit {
     [PeriodWidths.YEAR, 'per år'],
   ]);
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+ // @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChildren(MatPaginator) paginator = new QueryList<MatPaginator>();
   dataList: Map<Filter, MatTableDataSource<DataPoint>>;
 
   // The selected datapoints
@@ -242,7 +243,6 @@ export class HealthListItemsComponent implements OnInit {
       this.dataList = new Map<Filter, MatTableDataSource<DataPoint>>();
       for (let [filter, points] of this.conveyor.getDataList(this.selectedCategory).getPoints().entries()) {
         this.dataList.set(filter, new MatTableDataSource<DataPoint>(points));
-        //this.dataList.get(filter).paginator = this.paginator;
       }
       this.options = new Map<string, DataTypeCodedTextOpt[]>();
       this.selection.clear();
@@ -256,6 +256,14 @@ export class HealthListItemsComponent implements OnInit {
           this.options.set(key, datatypes.options);
         }
       }
+    }
+  }
+
+  ngAfterViewInit() {
+    let i = 0;
+    for (let table of this.dataList.values()) {
+      table.paginator = this.paginator.toArray()[i];
+      i++;
     }
   }
 
