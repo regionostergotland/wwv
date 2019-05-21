3bcefe0cf8a4516e0c4057f0040611af975a8a6a
AlexVestin <aleve030@student.liu.se>
2019-05-21 15:21:17 +0200

    Update getDataType function

--- src/app/gui/add-data-point/add-data-point.component.ts
+++ src/app/gui/add-data-point/add-data-point.component.ts
@@ -60,7 +60,7 @@ export class AddDataPointComponent implements OnInit {
     for (const [key, value] of Array.from(this.dataPoint.entries())) {
       const controller = this.getFormControl(key);
       controller.setValue(value);
-      if (this.requiredFields.includes(key)) {
+      if (this.requiredFields.includes(key) || key === 'date' || key.startsWith('period_')) {
         controller.disable();
       }
       this.pointData.set(key, value);
