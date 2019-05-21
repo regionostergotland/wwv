f28e28a23df83cb93a2aee6974e168b706660b57
AlexVestin <aleve030@student.liu.se>
2019-05-21 09:58:39 +0200

    Fix progress bar too large in production build, allow navigation to editor

--- src/app/gui/progress-bar/progress-bar.component.ts
+++ src/app/gui/progress-bar/progress-bar.component.ts
@@ -1,22 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Conveyor } from '../../conveyor.service';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
-  styleUrls: ['./progress-bar.component.scss']
+  styleUrls: ['./progress-bar.component.scss'],
 })
 export class ProgressBarComponent implements OnInit {
-
-  constructor(private conveyor: Conveyor, private router: Router) {
+  constructor(private conveyor: Conveyor, private router: Router) {}
+  onSmallScreen(): boolean {
+    return window.matchMedia('(max-width: 599px)').matches;
+  }
+/**
+ * @param buttonStep what step the button corresponds to.
+ * @returns css classes that should be applied to button
+ */
+  getClass(buttonStep: number): string {
+    const step = this.getCurrentStep();
+    if (buttonStep === step) {
+      return 'dot active';
+    } else if (step > buttonStep) {
+      return 'dot done';
+    }
+    return 'dot';
   }
 
-  ngOnInit() {
+  /**
+   * 
+   * @param to path to navigate to
+   * @param buttonStep what step the button button pressed corresponds to.
+   */
+  navigate(to: string, buttonStep: number): void {
+    if (buttonStep !== this.getCurrentStep() && to !== '') {
+      this.router.navigateByUrl('/' + to);
+    }
   }
-
+  ngOnInit() {}
   getCurrentStep(): number {
-    switch  (this.router.url) {
+    switch (this.router.url) {
       case '/sources': {
         return 1;
       }
@@ -37,5 +58,4 @@ export class ProgressBarComponent implements OnInit {
       }
     }
   }
-
 }
