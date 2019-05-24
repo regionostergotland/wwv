893c93cb9cd4662ea3e3b870d4b9115a5a7a05b2
Noah Hellman <noah.hellman@protonmail.com>
2019-05-24 11:10:36 +0200

    rm HelpModule, mv component to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
-/* Main component */
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
-/* Page / view components */
-import { InfoPageComponent } from './gui/info-page/info-page.component';
-import { HomePageComponent } from './gui/home-page/home-page.component';
-import { HelpModule } from './gui/help-page/HelpModule';
-import {
-  PlatformSelectionComponent
-} from './gui/platform-selection/platform-selection.component';
-import {
-  CategorySelectionComponent
-} from './gui/category-selection/category-selection.component';
-import {
-  BottomSheetCategoriesComponent,
-  EditorViewComponent,
-} from './gui/editor-view/editor-view.component';
-import {
-  InspectionViewComponent
-} from './gui/inspection-view/inspection-view.component';
-
-/* Static components */
-import { ProgressBarComponent } from './gui/progress-bar/progress-bar.component';
-import { FooterComponent } from './gui/footer/footer.component';
-import { ToolbarComponent } from './gui/toolbar/toolbar.component';
-
-import {
-  MAT_DATE_LOCALE
-} from '@angular/material';
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { DataViewerModule } from './gui/data-viewer/data-viewer.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AmazingTimePickerModule } from 'amazing-time-picker';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AddNewDataModalComponent } from './gui/editor-view/add-new-data-modal.component';
-
 import {
   MatCardModule,
   MatButtonModule,
@@ -80,16 +43,57 @@ import {
   MatFormFieldModule,
   MatTableModule,
 } from '@angular/material';
+import { MAT_DATE_LOCALE } from '@angular/material';
+import { AmazingTimePickerModule } from 'amazing-time-picker';
 
+/* Main component */
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
-import { CustomGoogleApiModule,  GoogleApiService, GoogleAuthService, } from './google-fit-config';
+/* Page / view components */
+import { InfoPageComponent } from './gui/info-page/info-page.component';
+import { HomePageComponent } from './gui/home-page/home-page.component';
+import { HelpPageComponent } from './gui/help-page/help-page.component';
 
+/* Fetching */
+import {
+  PlatformSelectionComponent
+} from './gui/platform-selection/platform-selection.component';
+import {
+  CategorySelectionComponent
+} from './gui/category-selection/category-selection.component';
+
+/* Editing */
+import {
+  BottomSheetCategoriesComponent,
+  EditorViewComponent,
+} from './gui/editor-view/editor-view.component';
+import { AddNewDataModalComponent } from './gui/editor-view/add-new-data-modal.component';
+
+/* Inspection */
+import {
+  InspectionViewComponent
+} from './gui/inspection-view/inspection-view.component';
+
+/* Common components */
+import { DataViewerModule } from './gui/data-viewer/data-viewer.module';
+
+/* Static components */
+import { ProgressBarComponent } from './gui/progress-bar/progress-bar.component';
+import { FooterComponent } from './gui/footer/footer.component';
+import { ToolbarComponent } from './gui/toolbar/toolbar.component';
+
+/* Google Fit conifguration */
+import { CustomGoogleApiModule,
+         GoogleApiService,
+         GoogleAuthService, } from './google-fit-config';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     InfoPageComponent,
+    HelpPageComponent,
     ProgressBarComponent,
     FooterComponent,
     PlatformSelectionComponent,
@@ -157,7 +161,6 @@ import { CustomGoogleApiModule,  GoogleApiService, GoogleAuthService, } from './
     MatGridListModule,
     BrowserModule,
     AppRoutingModule,
-    HelpModule,
     HttpClientModule,
     CustomGoogleApiModule,
     DataViewerModule,
